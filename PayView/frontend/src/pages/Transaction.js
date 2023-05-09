9f7dd8f0006b385c02903638b36f8ca5a5141d0a
Raul <raulvergara025@gmail.com>
2023-05-09 08:54:00 -0500

    Consultar saldo conectado

---
+++ PayView/frontend/src/pages/Transaction.js
@@ -0,0 +1,15 @@
+const Transaction = () => {
+    // Hacer como 15 states
+
+    // Hacer el POST request a partir de los valores ingresados en estos states
+
+    // Agregar somehow esto al historial del usuario
+
+    return ( 
+        <form>
+
+        </form>
+     );
+}
+ 
+export default Transaction;
--- PayView/frontend/src/pages/View.js
+++ PayView/frontend/src/pages/View.js
@@ -1,9 +1,44 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 const View = () => {
-    
+
+    const [cardNumber, setCardNumber] = useState('')
+    const [balance, setBalance] = useState('000')
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const response = await fetch('/api/user/cardInfo/' + cardNumber)
+        const json = await response.json()
+
+        if (response.ok) {
+            console.log(json)
+            setBalance(json.balance)
+            // Luego aqui va a haber un setHistorial(json.historial) pero todavia no se ha agregado el historial al backend ni al API
+        }
+        
+
+    }
 
 
     return ( 
-        <h2>Consulta de saldo</h2>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <h2>Consultar saldo</h2>
+                <label>Numero de tarjeta</label>
+                <input type="text" onChange={(e) => {setCardNumber(e.target.value)}}/>
+                <button>Consultar</button>
+            </form>
+            <div>
+                <h4>Saldo: {balance}</h4>
+                <h5>Historial: </h5>
+            </div>
+            <div>
+                <button onClick={navigate('/transaction')}>Realizar una transacción</button>
+            </div>
+        </div>
      );
 }
  
