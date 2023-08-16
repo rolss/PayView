7109a136badfcea4cb7a01dd4304009bceeead82
Raul <raulvergara025@gmail.com>
2023-08-15 20:05:34 -0500

    Reduced coupling in for Cards Updated View page for readability and better use of component nature

--- PayView/frontend/src/components/Cards.js
+++ PayView/frontend/src/components/Cards.js
@@ -1,63 +1,4 @@
-import { useEffect, useState, useSyncExternalStore } from "react"
-import NewCard from "../components/NewCard"
-import History from "../components/History"
-
-const Cards = ({user, eastAvailable, westernAvailable, fullUnavailable}) => {
-    const [error, setError] = useState(null)
-
-    const [westernCards, setWesternCards] = useState('')
-    const [eastCards, setEastCards] = useState('')
-
-    // Fetch all cards linked to this account and store the information in state variables
-    useEffect(() => {
-        const fetchCards = async () => {
-            try {
-                const response1 = await fetch('/api/east/query/fetchCards', {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${user.token}`
-                    }
-                })
-                const json1 = await response1.json()
-                
-                if (response1.ok) {
-                    setEastCards(json1.cards)
-                }
-
-                if (!response1.ok) {
-                    setError(json1.error)
-                }
-            } catch (error) {
-                console.error('Error at fetchCards:', error.message)
-            }
-
-            try {
-
-                const response2 = await fetch('/api/western/query/fetchCards', {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${user.token}`
-                    }
-                })
-                const json2 = await response2.json()
-                console.log("Cards2 json", json2.cards)
-                
-                if (response2.ok) {
-                    setWesternCards(json2.cards)
-                }
-
-                if (!response2.ok) {
-                    setError(json2.error)
-                }
-                
-            } catch (error) {
-                console.error('Error at fetchCards:', error.message)
-            }
-        }
-
-        fetchCards()
-        
-    } , [])
+const Cards = ({user, westernCards, eastCards, setWesternCards, setEastCards, error, setError}) => {
 
     //Ask backend to unlink a card from current account through cards ID
     const handleDelete = async (cardId, bank) => {
@@ -94,83 +35,59 @@ const Cards = ({user, eastAvailable, westernAvailable, fullUnavailable}) => {
     }
     
     return ( 
-        <div className="container-fluid mx-md-4">
-            <div className="mt-5 ms-sm-4 ps-sm-2">
-                {/* errors will be displayed only if either of the two bank query microservices is down */}
-                {eastAvailable === false && (
-                    <div className="error">
-                        <p className="fw-semi-bold"><i className="bi bi-exclamation-circle-fill me-2"></i>East Bank servers for obtaining your information are currently unavailable</p>
-                    </div>
-                )}
-                {westernAvailable === false && (
-                    <div className="error">
-                        <p className="fw-semi-bold"><i className="bi bi-exclamation-circle-fill me-2"></i>Western Bank servers for obtaining your information are currently unavailable</p>
-                    </div>
-                )}
-            </div>
-            <div className="row mt-5 ms-sm-3">
-                <div className="me-xl-5 me-md-3 col-12 col-md-7 col-lg-8 px-xl-4">
-                    <h2 className="mb-4">Cards</h2>
-                    {error && 
-                    <div className="error">
-                        <p>{error}</p>
-                    </div>}
-                    {(westernCards || eastCards) && 
-                    <table className="table mb-5">
-                        <thead>
-                            <tr>
-                                <th>Card</th>
-                                <th>Brand</th>
-                                <th>Balance</th>
-                                <th className="d-sm-block d-none">Bank</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {eastCards && eastCards.map((item) => (
-                                <tr key={item._id}>
-                                    <td>{item.cardNumber}</td>
-                                    <td>{item.company}</td>
-                                    <td>{item.balance}</td>
-                                    <td className="d-sm-block d-none">{item.bank}</td>
-                                    <td>
-                                        <button className="btn btn-sm btn-outline-danger" onClick={() => handleDelete(item._id, item.bank)}>Unlink</button>
-                                    </td>
-                                </tr>
-                            ))
-                            }
-                            {westernCards && westernCards.map((item) => (
-                                <tr key={item._id}>
-                                    <td>{item.cardNumber}</td>
-                                    <td>{item.company}</td>
-                                    <td>{item.balance}</td>
-                                    <td className="d-sm-block d-none">{item.bank}</td>
-                                    <td>
-                                        <button className="btn btn-sm btn-outline-danger" onClick={() => handleDelete(item._id, item.bank)}>Unlink</button>
-                                    </td>
-                                </tr>
-                            ))
-                            }
-                        </tbody>
-                    </table>
+        <>
+            <h2 className="mb-4">Cards</h2>
+            {error && 
+            <div className="error">
+                <p>{error}</p>
+            </div>}
+            {(westernCards || eastCards) && 
+            <table className="table mb-5">
+                <thead>
+                    <tr>
+                        <th>Card</th>
+                        <th>Brand</th>
+                        <th>Balance</th>
+                        <th className="d-sm-block d-none">Bank</th>
+                    </tr>
+                </thead>
+                {/* Display East cards piled on top of Western cards */}
+                <tbody>
+                    {eastCards && eastCards.map((item) => (
+                        <tr key={item._id}>
+                            <td>{item.cardNumber}</td>
+                            <td>{item.company}</td>
+                            <td>{item.balance}</td>
+                            <td className="d-sm-block d-none">{item.bank}</td>
+                            <td>
+                                <button className="btn btn-sm btn-outline-danger" onClick={() => handleDelete(item._id, item.bank)}>Unlink</button>
+                            </td>
+                        </tr>
+                    ))
                     }
-
-                    {/* Message displayed when the backend returns no cards linked to current user */}
-                    {(!westernCards && !eastCards) && 
-                        <div className="mb-5">
-                            <p>No cards found. Please add a card or try again later</p>
-                        </div>
+                    {westernCards && westernCards.map((item) => (
+                        <tr key={item._id}>
+                            <td>{item.cardNumber}</td>
+                            <td>{item.company}</td>
+                            <td>{item.balance}</td>
+                            <td className="d-sm-block d-none">{item.bank}</td>
+                            <td>
+                                <button className="btn btn-sm btn-outline-danger" onClick={() => handleDelete(item._id, item.bank)}>Unlink</button>
+                            </td>
+                        </tr>
+                    ))
                     }
+                </tbody>
+            </table>
+            }
 
-                    <History user={user} initial={true}/>
+            {/* Message displayed when the backend returns no cards linked to current user */}
+            {(!westernCards && !eastCards) && 
+                <div className="mb-5">
+                    <p>No cards found. Please add a card or try again later</p>
                 </div>
-                
-                <div className="col-8 col-sm-6 col-md-4 col-lg-3 px-xl-4 mt-5 mt-md-0 mb-4 mb-md-0">
-                    <div>
-                        <NewCard user={user} setEastCards={setEastCards} setWesternCards={setWesternCards} />
-                    </div>
-                </div>
-            </div>
-        </div>
+            }
+        </>
      );
 }
  
--- PayView/frontend/src/pages/View.js
+++ PayView/frontend/src/pages/View.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react"
 import { useAuthContext } from "../hooks/useAuthContext"
 import Cards from "../components/Cards"
+import History from "../components/History"
+import NewCard from "../components/NewCard"
 
 
 const View = () => {
@@ -8,6 +10,10 @@ const View = () => {
     
     const [eastAvailable, setEastAvailable] = useState(true)
     const [westernAvailable, setWesternAvailable] = useState(true)
+    const [westernCards, setWesternCards] = useState('')
+    const [eastCards, setEastCards] = useState('')
+
+    const [error, setError] = useState(null)
 
 
     useEffect(() => {
@@ -54,11 +60,91 @@ const View = () => {
         checkWesternAvailability()
     }, [user.token])
 
-    // !!Add: loading screen to history
+    // Fetch all cards linked to this account and store the information in state variables
+    useEffect(() => {
+        const fetchCards = async () => {
+            try {
+                const response1 = await fetch('/api/east/query/fetchCards', {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${user.token}`
+                    }
+                })
+                const json1 = await response1.json()
+                
+                if (response1.ok) {
+                    setEastCards(json1.cards)
+                }
+
+                if (!response1.ok) {
+                    setError(json1.error)
+                }
+            } catch (error) {
+                console.error('Error at fetchCards:', error.message)
+            }
+
+            try {
+
+                const response2 = await fetch('/api/western/query/fetchCards', {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${user.token}`
+                    }
+                })
+                const json2 = await response2.json()
+                console.log("Cards2 json", json2.cards)
+                
+                if (response2.ok) {
+                    setWesternCards(json2.cards)
+                }
+
+                if (!response2.ok) {
+                    setError(json2.error)
+                }
+                
+            } catch (error) {
+                console.error('Error at fetchCards:', error.message)
+            }
+        }
+
+        fetchCards()
+        
+    } , [])
+
     return ( 
-        <div>    
-            <Cards user={user} eastAvailable={eastAvailable} westernAvailable={westernAvailable} fullUnavailable={!eastAvailable&&!westernAvailable}/>
+     <div className="container-fluid mx-md-4">
+        
+        <div className="mt-5 ms-sm-4 ps-sm-2">
+            {/* errors will be displayed only if either of the two bank query microservices is down */}
+            {eastAvailable === false && (
+                <div className="error">
+                    <p className="fw-semi-bold"><i className="bi bi-exclamation-circle-fill me-2"></i>East Bank servers for obtaining your information are currently unavailable</p>
+                </div>
+            )}
+            {westernAvailable === false && (
+                <div className="error">
+                    <p className="fw-semi-bold"><i className="bi bi-exclamation-circle-fill me-2"></i>Western Bank servers for obtaining your information are currently unavailable</p>
+                </div>
+            )}
         </div>
+ 
+        <div className="row mt-5 ms-sm-3"> 
+            <div className="me-xl-5 me-md-3 col-12 col-md-7 col-lg-8 px-xl-4">   
+                <Cards 
+                user={user} 
+                westernCards={westernCards} eastCards={eastCards}
+                setWesternCards={setWesternCards} setEastCards={setEastCards}
+                error={error} setError={setError}
+                />
+                <History user={user} initial={true}/>
+            </div>
+            <div className="col-8 col-sm-6 col-md-4 col-lg-3 px-xl-4 mt-5 mt-md-0 mb-4 mb-md-0">
+                <div>
+                    <NewCard user={user} setEastCards={setEastCards} setWesternCards={setWesternCards} />
+                </div>
+            </div>
+        </div>
+    </div>
      );
 }
  
